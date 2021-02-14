c01082c7d8982fae6712bacf3f84f5297d760ebf
griesba <griesba.pa@hotmail.com>
2021-02-14 22:24:53 +0100

    Project set up

---
+++ src/test/java/com/griesba/brewery/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griesba.brewery.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
